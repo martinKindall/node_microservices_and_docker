9d88f065f0c9ef160ad67ab76b66370498c53475
martinKindall <mart256@gmail.com>
2020-08-13 21:27:21 -0400

    Added bodyparser and properly reading post request

--- api/app.js
+++ api/app.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json());
 
 app.get('/orders', (req, res) => {
   res.json({"message": "I was read!"});
+});
+
+app.post('/orders', (req, res) => {
+  const newOrder = req.body;
+  res.json(newOrder);
 });
 
 app.listen(3000, function () {
