b2ed94961b99021c1dafdf6be6f1ecaae4c91a10
martinKindall <mart256@gmail.com>
2020-08-16 23:49:45 -0400

    Adding express to db microservice.

--- api/app.js
+++ api/app.js
@@ -7,5 +7,5 @@ app.use(bodyParser.json());
 require('./routes')(app);
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Orders app listening on port 3000!');
 });
--- db/app.js
+++ db/app.js
@@ -1,4 +1,22 @@
 const TopicSubscriber = require("./TopicSubscriber.js");
 const Mongo = require("./MongoService.js");
 
-TopicSubscriber.subscribeToBrokerAndPersistNewOrders(new Mongo.MongoService());
+const dbService = new Mongo.MongoService();
+TopicSubscriber.subscribeToBrokerAndPersistNewOrders(dbService);
+
+var express = require('express');
+var app = express();
+
+app.get('/orders', (req, res) => {
+    dbService.readOrders()
+    .then(orders => {
+        res.json(orders);
+    })
+    .catch(err => {
+        res.status(500).json({"message": JSON.stringify(err)});
+    })
+});
+
+app.listen(80, function () {
+  console.log('DB microservice listening on port 80!');
+});
